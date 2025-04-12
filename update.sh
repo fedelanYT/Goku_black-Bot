81fbb34f87838402a55950c463bf124ee6babd65
Fede <vigevanif@yahoo.com>
2025-04-12 10:11:09 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
-# Código desarrollado por @gata_dios 
+# Código desarrollado por @fedelan 
   # * Al bifurcar este repositorio, dejen los créditos a todos los creadores de cada código por favor. ♥
 #!/data/data/com.termux/files/usr/bin/bash 
-BOT_DIR="Goku-Black-Bot-MD"  
-BOT_REPO="https://github.com/Eliasivan/$BOT_DIR"
+BOT_DIR="Goku-Black-MD"  
+BOT_REPO="https://github.com/fedelan/$BOT_DIR"
 DB_FILE="database.json"
 
 GREEN='\033[32m'
