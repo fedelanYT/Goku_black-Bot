0946b487d5541df3dfdfb09151a39121651918c4
Fede <vigevanif@yahoo.com>
2025-04-12 10:20:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Goku-Black-Bot\nMD', {
+say('Goku-Black\nMD', {
 font: 'block',
 align: 'center',
 colors: ['red']
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['red']
 });
-say(`Developed By • Ivan`, {
+say(`Developed By • Fede`, {
 font: 'console',
 align: 'center',
 colors: ['yellow']
