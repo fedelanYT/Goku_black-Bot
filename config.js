286d7771fceb4d9dfeed458a036e4965f70d3d13
Fedelan'sYT <federicovigevani999@gmail.com>
2025-04-16 08:39:37 -0300

    Update config.js

--- config.js
+++ config.js
@@ -102,7 +102,7 @@ global.channel = 'https://whatsapp.com/channel/0029Vb5oaHFCBtxIGWefdp0n' //Canal
 global.yt = 'https://youtube.com/@Fedelan_v13' //Canal De Youtube
 global.md = 'https://github.com/fedelanYT' //Github Oficial
 global.correo = 'vigevanif@yahoo.com'
-global.cn ='';
+global.cn ='https://whatsapp.com/channel/0029Vb5oaHFCBtxIGWefdp0n' //menu;
 
 var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'Linda Mañana'; break; case 1: hour = 'Linda Mañana'; break; case 2: hour = 'Linda Mañana'; break; case 3: hour = 'Linda Mañana'; break; case 4: hour = 'linda mañana'; break; case 5: hour = 'Linda Mañana'; break; case 6: hour = 'Linda Mañana'; break; case 7: hour = 'Linda Mañana'; break; case 8: hour = 'Linda Mañana'; break; case 9: hour = 'Linda Mañana'; break; case 10: hour = 'Lindo Dia'; break; case 11: hour = 'Lindo Dia'; break; case 12: hour = 'Lindo Dia'; break; case 13: hour = 'Lindo Dia'; break; case 14: hour = 'Linda Tarde'; break; case 15: hour = 'Linda Tarde'; break; case 16: hour = 'Linda Tarde'; break; case 17: hour = 'Linda Tarde'; break; case 18: hour = 'Linda Noche'; break; case 19: hour = 'Linda Noche'; break; case 20: hour = 'Linda Noche'; break; case 21: hour = 'Linda Noche'; break; case 22: hour = 'Linda Noche'; break; case 23: hour = 'Linda Noche'; break;}
 global.saludo = '🍭' + hour;
